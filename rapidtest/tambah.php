8aa539fb593767b61b640511ae2c804bc16c34b3
MatthewKevin <99637930+MatthewKevin@users.noreply.github.com>
2022-03-22 14:09:48 +0700

    Update tambah.php

--- rapidtest/tambah.php
+++ rapidtest/tambah.php
@@ -20,3 +20,9 @@ function tampilkanFormulirTambahPeserta(){
     require_once 'view/view-tambah-peserta.php';
 }
 
+function kirimKeHalamanUtama($pesan){
+    header("Location: index.php?pesan=$pesan");
+    $pesan = "Peserta berhasil ditambah.";
+}
+
+
